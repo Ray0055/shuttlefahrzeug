dad8ea50c4ade935ec323e62b51488e5e8f24a99
Hu <am3s33@fkfs.de>
2023-11-16 21:55:38 +0100

    finished most work without bird eye view

--- lane_assistant.cpp
+++ lane_assistant.cpp
@@ -28,21 +28,23 @@ public:
         return true;
     }
 
-    void processImage( cv::Mat image )
-    {
-        cv::Mat processd_img;
-        processd_img = regionDetection( image );
-        processd_img = colorDetection( processd_img );
+    // void processImage( cv::Mat image )
+    //{
+    //    cv::Mat processd_img;
+    //     regionDetection( image );
 
-        processd_img = edgeDetection( processd_img );
-        processd_img = lineDetection( processd_img );
-        imshow( "lane detection", processd_img );
-        waitKey( 0 );
-    }
+    //    processd_img = colorDetection( processd_img );
+
+    //    processd_img = edgeDetection( processd_img );
+
+    //    // processd_img = lineDetection( processd_img );
+    //    imshow( "lane detection", processd_img );
+    //    waitKey( 0 );
+    //}
 
 protected:
     std::string image_name_;
-    cv::Mat image_;
+    cv::Mat image_, original_image_;
     tronis::LocationSub ego_location_;
     tronis::OrientationSub ego_orientation_;
     double ego_velocity_;
@@ -51,9 +53,15 @@ protected:
     // Insert your algorithm here
     void detectLanes()
     {
-        // edgeDetection( image_ );
-        // lineDetection();
-        // do stuff
+        cout << "Start to detect lines: " << endl;
+
+        regionDetection( image_ );
+
+        colorDetection( image_ );
+
+        edgeDetection( image_ );
+
+        lineDetection( image_ );
     }
 
     bool processPoseVelocity( tronis::PoseVelocitySub* msg )
@@ -77,9 +85,10 @@ protected:
         cv::inRange( hsv, lower, upper, dst );
     }
 
-    Mat regionDetection( Mat img )
+    void regionDetection( Mat img )
     {
-        Mat result, mask_three_channel;
+        cout << "Start to detect region: " << endl;
+        Mat result;
         int img_height = img.size().height;
         int img_width = img.size().width;
         /// mask image with polygon
@@ -87,10 +96,10 @@ protected:
         /// create cone shape
         vector<Point> pts;
         vector<vector<Point>> v_pts;
-        pts.push_back( Point( 0, img_height / 2 + 90 ) );
-        pts.push_back( Point( img_width / 2, img_height / 2 + 45 ) );
-        pts.push_back( Point( img_width, img_height / 2 + 90 ) );
-        pts.push_back( Point( img_width, img_height - 60 ) );
+        pts.push_back( Point( 0, img_height / 2 + 45 ) );              // left point
+        pts.push_back( Point( img_width / 2, img_height / 2 + 10 ) );  // middle point
+        pts.push_back( Point( img_width, img_height / 2 + 45 ) );      // right high point
+        pts.push_back( Point( img_width, img_height - 60 ) );          // right low point
         pts.push_back( Point( img_width * 0.7, img_height - 125 ) );
         pts.push_back( Point( img_width * 0.3, img_height - 125 ) );
         pts.push_back( Point( 0, img_height - 60 ) );
@@ -98,15 +107,26 @@ protected:
         /// add cone to mask
         fillPoly( mask_crop, v_pts, 255 );
 
-        cvtColor( mask_crop, mask_three_channel,
-                  COLOR_GRAY2BGR );  // transform mask into three channels
+        Mat mask_three_channel( mask_crop.size(), CV_8UC4 );  // why here should be four channels
+
+        vector<Mat> channels( 4 );
+        channels[0] = mask_crop;
+        channels[1] = mask_crop;
+        channels[2] = mask_crop;
+        channels[3] = mask_crop;
+        merge( channels, mask_three_channel );
+
+        // cv::cvtColor( mask_crop, mask_three_channel,
+        //              COLOR_GRAY2BGR );  // transform mask into three channels
 
         cv::bitwise_and( img, mask_three_channel, result );
-        // image_orig.copyTo( temp, mask_crop2 ); //if color channel and mask
 
-        return result;
+        // image_orig.copyTo( temp, mask_crop2 ); //if color channel and mask
+        cout << " Region is detected." << endl;
+
+        image_ = result;
     }
-    Mat colorDetection( cv::Mat& img )
+    void colorDetection( cv::Mat img )
     {
         Mat mask_yellow, mask_white, mask_combined;
 
@@ -116,14 +136,15 @@ protected:
         colorThreshold( img, mask_yellow, lower_yellow, upper_yellow );
 
         // white region hsv threshold
-        cv::Scalar lower_white( 0, 20, 60 );
-        cv::Scalar upper_white( 180, 70, 255 );
+        cv::Scalar lower_white( 106, 0, 120 );
+        cv::Scalar upper_white( 173, 90, 230 );
         colorThreshold( img, mask_white, lower_white, upper_white );
 
         cv::bitwise_or( mask_yellow, mask_white, mask_combined );
-        return mask_combined;
+        cout << " Color is detected." << endl;
+        image_ = mask_combined;
     }
-    Mat edgeDetection( cv::Mat image )
+    void edgeDetection( cv::Mat image )
     {
         cv::Mat grad_img, blurred_img;
         /* Solber function
@@ -153,66 +174,86 @@ cv::addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
         cv::GaussianBlur( image, blurred_img, cv::Size( 3, 3 ), 20, 0, cv::BORDER_DEFAULT );
 
         cv::Canny( image, grad_img, 50, 200 );
-
-        return grad_img;
+        cout << " Edge is detected." << endl;
+        image_ = grad_img;
     }
 
-    Mat lineDetection( cv::Mat img )
+    void lineDetection( cv::Mat img )
     {
         Mat img_color;
         std::vector<Vec4i> lines;
-        cv::HoughLinesP( img, lines, 5, CV_PI / 60, 40, 120, 10 );
+        cv::HoughLinesP( img, lines, 6, CV_PI / 90, 100, 40, 20 );
 
         cvtColor( img, img_color, COLOR_GRAY2BGR );
 
-        // painting lines in original image
-        /*for( size_t i = 0; i < lines.size(); i++ )
+        //// painting lines in original image
+        // for( size_t i = 0; i < lines.size(); i++ )
+        //{
+        //    Vec4i l = lines[i];
+        //
+        //    line( img_color, Point( l[0], l[1] ), Point( l[2], l[3] ), Scalar( 0, 0, 255 ), 3,
+        //          LINE_AA );
+        //}
+
+        vector<Point> left_points, right_points;
+
+        for( const Vec4i& l : lines )
         {
-            Vec4i l = lines[i];
-			
-            line( img_color, Point( l[0], l[1] ), Point( l[2], l[3] ), Scalar( 0, 0, 255 ), 3,
-                  LINE_AA );
-        }*/
+            double slope = static_cast<double>( l[3] - l[1] ) / ( l[2] - l[0] );
 
-
-
-        return img_color;
-    }
-
-    vector<Point2f> mergeLine( vector<Vec4i> lines )
-    {
-        vector<Point2f> points;
-
-		//Divide lines into right and left
-
-
-
-        for( size_t i = 0; i < lines.size() ;i++ )
-        {
-            Vec4i l = lines[i];
-            points.push_back( Point2f( l[0], l[1] ) );
-            points.push_back( Point2f( l[2], l[3] ) );
+            // According to positive/negative of slope, seperate points into two groups
+            if( slope > 0 )
+            {
+                right_points.emplace_back( l[0], l[1] );
+                right_points.emplace_back( l[2], l[3] );
+            }
+            else
+            {
+                left_points.emplace_back( l[0], l[1] );
+                left_points.emplace_back( l[2], l[3] );
+            }
         }
 
-        Vec4i line_parameter;
-        fitLine( points, line_parameter, DIST_L2, 0, 0.01, 0.01 );
-        float vx = line_parameter[0];
-        float vy = line_parameter[1];
-        float x = line_parameter[2];
-        float y = line_parameter[3];
+        
+        image_ = original_image_;
+        int imgHeight = img_color.rows;
+        // merge and draw left lane
+        if( !left_points.empty() )
+        {
+            mergeLines( original_image_, left_points, Scalar( 255, 0, 0 ), 10000.0, imgHeight );
+        }
 
-        Point2f line_point1, line_point2;
-        line_point1.x = x - vx * 100;
-        line_point1.y = y - vy * 100;
-        line_point2.x = x + vx * 100;
-        line_point2.y = y + vy * 100;
-
-		points[0] = line_point1;
-        points[1] = line_point2;
-
-		return points;
-
+        // merge and draw right lane
+        if( !right_points.empty() )
+        {
+            mergeLines( original_image_, right_points, Scalar( 0, 255, 0 ), 10000.0, imgHeight );
+        }
     }
+
+    void mergeLines( Mat& image, const vector<Point>& points, Scalar color, double limitedLength,
+                     int img_height )
+    {
+        Vec4f line_parameters;
+        fitLine( points, line_parameters, DIST_L2, 0, 0.01, 0.01 );
+
+        Point point1, point2;
+        point1.x = line_parameters[2];
+        point1.y = line_parameters[3];
+        double k = line_parameters[1] / line_parameters[0];
+
+        //
+        int startY = img_height / 2 + 40;
+        point1.x = point1.x + ( startY - point1.y ) / k;
+        point1.y = startY;
+
+        //
+        point2.x = point1.x + ( img_height - point1.y ) / k;
+        point2.y = img_height;
+
+        //
+        line( image, point1, point2, color, 2, LINE_AA );
+    }
+
     // Helper functions, no changes needed
 public:
     // Function to process received tronis data
@@ -299,6 +340,7 @@ protected:
     // Function to convert tronis image to openCV image
     bool processImage( const std::string& base_name, const tronis::Image& image )
     {
+        cout << " Start to process image. " << endl;
         if( image.empty() )
         {
             std::cout << "empty image" << std::endl;
@@ -307,13 +349,13 @@ protected:
 
         image_name_ = base_name;
         image_ = tronis::image2Mat( image );
-
+        original_image_ = image_;
         detectLanes();
         // showImage( image_name_, detectLanes(image_) );
         showImage( image_name_, image_ );
     }
 };
-/*
+
 // main loop opens socket and listens for incoming data
 int main( int argc, char** argv )
 {
@@ -391,10 +433,10 @@ int main( int argc, char** argv )
     }
     return 0;
 }
-*/
-int main( int argc, char** argv )
-{
-    Mat original_img = imread( "C:\\Users\\am3s33\\Pictures\\Camera Roll\\lane1.png" );
-    LaneAssistant laneAssistant;
-    laneAssistant.processImage( original_img );
-}
+
+// int main( int argc, char** argv )
+//{
+//    Mat original_img = imread( "C:\\Users\\am3s33\\Pictures\\Camera Roll\\lane2.png" );
+//    LaneAssistant laneAssistant;
+//    laneAssistant.processImage( original_img );
+//}
